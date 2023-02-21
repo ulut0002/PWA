1dc2e74b90ae940056b2a428f5c488ad31ffbcaf
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-02-21 16:09:45 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const APP = {
+  currentPage: 'home',
+  init() {
+    //page has loaded
+  },
+  addListeners() {
+    //add DOM listeners
+  },
+  navigate(page) {
+    //navigate to a new page
+  },
+};
+
+document.addEventListener('DOMContentLoaded', APP.init);
