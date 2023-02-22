2358f7da1848a14f8f6d199dc786c41dc88d2dfb
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-02-22 12:25:03 -0500

    Add test functions for the development of cache functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const APP = {
-  currentPage: 'home',
+  currentPage: "home",
   init() {
     //page has loaded
   },
@@ -11,4 +11,48 @@ const APP = {
   },
 };
 
-document.addEventListener('DOMContentLoaded', APP.init);
+document.addEventListener("DOMContentLoaded", APP.init);
+
+//################### TEST FUNCTIONS ################
+
+const test_UserArray = [];
+function testCreateDummyUsers() {
+  test_UserArray.push(test_createDummyUser("Jane Smith", "1969-03-03"));
+  test_UserArray.push(test_createDummyUser("Allison Cooper", "1976-04-05"));
+  test_UserArray.push(test_createDummyUser("Robert Duff", "1989-03-03"));
+  test_UserArray.push(test_createDummyUser("Jimmy Atkinson", "1977-02-09"));
+}
+function test_createDummyUser(name = "John Doe", dob = "1990-01-01") {
+  let person = {
+    id: crypto.randomUUID(),
+    name: name,
+    dob: Date.parse(dob),
+    gifts: [],
+  };
+  const numOfGifts = randomNum(2, 5);
+  for (let index = 0; index < numOfGifts; index++) {
+    person.gifts.push(test_createRandomGift());
+  }
+
+  return person;
+}
+
+function test_createRandomGift() {
+  const giftId = crypto.randomUUID();
+  return {
+    gift_id: giftId,
+    text: `Gift #${giftId.substring(0, 2).toUpperCase()}`,
+    store: `Ground ${giftId.substring(4, 7).toUpperCase()}`,
+    url: `https://www.ground${giftId.substring(4, 7)}.com`,
+  };
+}
+
+function randomNum(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// console.log("User: ", test_createDummyUser());
+testCreateDummyUsers();
+
+console.log(test_UserArray);
